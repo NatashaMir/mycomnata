6b28afeed4552122ac433570432b5208f84795a2
Nataliya Miroshnychenko <nataliya.miroshnychenko@gmail.com>
2021-03-23 13:14:19 +0200

    Add testAnswerMethodFalse in AnswerMethodTest

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,6 +1,9 @@
 package com.example.restservice;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,29 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+    private final Map<Integer, String> hmap = new ConcurrentHashMap<>();
 
-    private String answer = "";
     private static final String template = "Your short URL shorturl.com/%s";
     private static final String template_answer = "Your full URL %s";
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name){
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name) {
         hmap.put(name.hashCode(), name);
         return new Greeting(String.format(template, name.hashCode()));
     }
 
     @GetMapping("/answer")
-    public Greeting answer(@RequestParam(value = "shortname", defaultValue = "") int shortname){
-        answer =  hmap.get(shortname);
+    public Greeting answer(@RequestParam(value = "shortname", defaultValue = "") int shortname) {
+        String answer = hmap.get(shortname);
         if (answer != null) {
             return new Greeting(String.format(template_answer, answer));
         } else {
-            if (hmap.containsKey(shortname)) {
-                return new Greeting(String.format(template_answer, ""));
-            } else {
-                return new Greeting(String.format(template_answer, "doesn`t exists"));
-            }
+            return new Greeting(String.format(template_answer, "doesn`t exists"));
         }
     }
 }
--- src/test/java/com/example/restservice/AnswerMethodTest.java
+++ src/test/java/com/example/restservice/AnswerMethodTest.java
@@ -3,6 +3,7 @@ package com.example.restservice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnswerMethodTest {
@@ -11,7 +12,7 @@ public class AnswerMethodTest {
     String url = "https://www.vogella.com/tutorials/JUnit/article.html";
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         greetingControllerTest = new GreetingController();
     }
 
@@ -23,18 +24,11 @@ public class AnswerMethodTest {
         assertEquals(new Greeting("Your full URL https://www.vogella.com/tutorials/JUnit/article.html"), greetingControllerTest.answer(hash));
     }
 
-//    @Test
-//    @DisplayName("Check hash is empty:")
-//    public void testAnswerMethodNull() {
-//        assertEquals(greetingControllerTest.greeting("https://www.vogella.com/tutorials/JUnit/article.html"), answerTest);
-//    }
-//
-//    @Test
-//    @DisplayName("Check hash doesn`t exist:")
-//    public void testAnswerMethodFalse() {
-//        assertEquals(greetingControllerTest.greeting("https://www.vogella.com/tutorials/JUnit/article.html"), answerTest);
-//    }
-
+    @Test
+    @DisplayName("Check hash doesn`t exist:")
+    public void testAnswerMethodFalse() {
+        assertEquals(new Greeting("Your full URL doesn`t exists"), greetingControllerTest.answer(123456789));
+    }
 
 
 }
