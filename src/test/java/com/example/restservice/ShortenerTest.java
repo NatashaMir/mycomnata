1a4aa4f1e1a40a9226a810e2289ad611637383ce
Nataliya Miroshnychenko <nataliya.miroshnychenko@gmail.com>
2021-04-14 13:14:20 +0300

    Added new methods test class ShortenerTest

--- src/test/java/com/example/restservice/ShortenerTest.java
+++ src/test/java/com/example/restservice/ShortenerTest.java
@@ -3,15 +3,30 @@ package com.example.restservice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.Matchers.hasLength;
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class ShortenerTest {
 
     @Test
-    @DisplayName("Check rendom string:")
+    @DisplayName("Check random string:")
     public void testShortenerMethod() {
         assertNotNull(new Shortener().encode("http://shorturl.com:8/answer/415207542"));
+    }
+
+    @Test
+    @DisplayName("Check random string sympols:")
+    public void testShortenerMethodCheckSymbols() {
+        assertThat(new Shortener().encode("http://shorturl.com:8/answer/415207542"), matchesPattern("[A-Za-z0-9]*"));
+    }
+
+    @Test
+    @DisplayName("Check random string length:")
+    public void testShortenerMethodCheckLength() {
+        assertThat(new Shortener().encode("http://shorturl.com:8/answer/415207542"), hasLength(10));
     }
 
 }
